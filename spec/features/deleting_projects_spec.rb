8c75819ca25b3bcd9426c262584f7d74a07f44b9
Tim DiSabatino <tim@pathwaysdigital.com>
2018-06-20 12:41:20 -0700

    Projects can now be deleted

---
+++ spec/features/deleting_projects_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "Users can delete projects" do
+  scenario "successfully" do
+    FactoryBot.create(:project, name: "Sublime Text 3")
+
+    visit "/"
+    click_link "Sublime Text 3"
+    click_link "Edit Project"
+
+    expect(page).to have_content "Project has been deleted."
+    expect(page.current_url).to eq projects_url
+    expect(page).to have_no_content "Sublime Text 3"
+  end
+end
