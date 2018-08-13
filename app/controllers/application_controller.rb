5ceaf5df59a608a2d533fdbb5ff94af292551347
Tim DiSabatino <tim@pathwaysdigital.com>
2018-08-13 05:47:23 -0700

    Verify that we use authorization on all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
+  after_action :verify_authorized, except: [:index],
+    unless: :devise_controller?
+  after_action :verify_policy_scoped, only: [:index],
+    unless: :devise_controller?
+
   protect_from_forgery
 
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
