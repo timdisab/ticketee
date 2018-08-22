d63b5a87552849618c051c01a1fba465e61318ad
Tim DiSabatino <tim@pathwaysdigital.com>
2018-08-22 05:44:04 -0700

    Uploaded attachments are now served through the AttachmentsController

---
+++ spec/features/viewing_attachments_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "Users can view a ticket's attached files" do
+  let(:user) { FactoryBot.create :user }
+  let(:project) { FactoryBot.create :project }
+  let(:ticket) { FactoryBot.create :ticket, project: project, author: user }
+  let!(:attachment) { FactoryBot.create :attachment, ticket: ticket,
+    file_to_attach: "spec/fixtures/speed.txt" }
+
+  before do
+    assign_role!(user, :viewer, project)
+    login_as(user)
+  end
+
+  scenario "successfully" do
+    visit project_ticket_path(project, ticket)
+    click_link "speed.txt"
+
+    expect(current_path).to eq attachment_path(attachment)
+    expect(page).to have_content "The blink tag can blink faster"
+  end
+end
