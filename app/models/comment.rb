d4b408db0c43a0e04932d11bbfd5ec2f005467d4
Tim DiSabatino <tim@pathwaysdigital.com>
2018-09-01 06:58:44 -0700

    Authorized users can now leave comments on tickets

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :ticket
-  belongs_to :author
+  belongs_to :author, class_name: "User", optional: true
+  scope :persisted, lambda { where.not(id: nil) }
 
   validates :text, presence: true
+  delegate :project, to: :ticket
 end
---
+++ spec/factories/comment_factory.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :comment do
+    text { "A comment describing some changes that should be made
+      to this ticket."}
+  end
+end
